e0abae2a32d955bdd43c22e06d3f202f39918db4
mikemacharia39 <mikemacharia39@gmail.com>
2022-07-03 22:26:46 +0300

    update order controller

--- src/main/java/com/mikehenry/interceptor/controller/OrderController.java
+++ src/main/java/com/mikehenry/interceptor/controller/OrderController.java
@@ -1,6 +1,8 @@
 package com.mikehenry.interceptor.controller;
 
 import com.mikehenry.interceptor.model.Order;
+import com.mikehenry.interceptor.service.OrderService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,11 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping(value = "order", consumes = MediaType.APPLICATION_JSON_VALUE)
 @RestController
+@RequiredArgsConstructor
 public class OrderController {
+
+    private final OrderService orderService;
 
     @PostMapping("create")
     public Order createOrder(@RequestBody Order order) {
-
+        orderService.createOrder(order);
         return order;
     }
 }
