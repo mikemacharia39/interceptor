44067685770f597c0da72b890159455eecf81ff4
mikemacharia39 <mikemacharia39@gmail.com>
2022-07-03 22:00:35 +0300

    implement order interceptor

--- src/main/java/com/mikehenry/interceptor/interceptor/OrderInterceptor.java
+++ src/main/java/com/mikehenry/interceptor/interceptor/OrderInterceptor.java
@@ -1,7 +1,28 @@
 package com.mikehenry.interceptor.interceptor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
 public class OrderInterceptor implements HandlerInterceptor {
 
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("PreHandle interceptor");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("PostHandle interceptor");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        log.info("After completion interceptor");
+    }
 }
