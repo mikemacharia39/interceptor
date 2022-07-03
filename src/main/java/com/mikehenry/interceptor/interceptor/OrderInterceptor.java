a5512583a58c250ebd2b163ad58c37a9a9ac4ef0
mikemacharia39 <mikemacharia39@gmail.com>
2022-07-03 22:28:35 +0300

    update order interceptor

--- src/main/java/com/mikehenry/interceptor/interceptor/OrderInterceptor.java
+++ src/main/java/com/mikehenry/interceptor/interceptor/OrderInterceptor.java
@@ -1,12 +1,14 @@
 package com.mikehenry.interceptor.interceptor;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@Component
 @Slf4j
 public class OrderInterceptor implements HandlerInterceptor {
 
