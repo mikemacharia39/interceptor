2a5ec485125ed75ef9ffc039d892127ab4d5d85f
mikemacharia39 <mikemacharia39@gmail.com>
2022-07-03 22:06:08 +0300

    implement interceptor configuration

--- src/main/java/com/mikehenry/interceptor/configuration/InterceptorConfig.java
+++ src/main/java/com/mikehenry/interceptor/configuration/InterceptorConfig.java
@@ -1,6 +1,13 @@
 package com.mikehenry.interceptor.configuration;
 
+import com.mikehenry.interceptor.interceptor.OrderInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class InterceptorConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new OrderInterceptor());
+    }
 }
