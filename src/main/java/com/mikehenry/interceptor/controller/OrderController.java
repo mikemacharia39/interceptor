cec9059eea41b400221e1d0d5f49894d02affc92
mikemacharia39 <mikemacharia39@gmail.com>
2022-07-03 21:54:37 +0300

    implement order controller

--- src/main/java/com/mikehenry/interceptor/controller/OrderController.java
+++ src/main/java/com/mikehenry/interceptor/controller/OrderController.java
@@ -1,11 +1,19 @@
 package com.mikehenry.interceptor.controller;
 
+import com.mikehenry.interceptor.model.Order;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping(value = "order", consumes = MediaType.APPLICATION_JSON_VALUE)
 @RestController
 public class OrderController {
 
-    public void createOrder() {
+    @PostMapping("create")
+    public Order createOrder(@RequestBody Order order) {
 
+        return order;
     }
 }
