4d506f334490f9ccfd5842c144e0486dee7f0b55
mikemacharia39 <mikemacharia39@gmail.com>
2022-07-03 21:17:37 +0300

    project setup

---
+++ src/test/java/com/mikehenry/interceptor/InterceptorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikehenry.interceptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterceptorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
