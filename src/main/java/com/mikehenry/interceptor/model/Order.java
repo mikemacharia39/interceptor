c513e1b6cf71e2a938bac544daa0db04b9c6f86e
mikemacharia39 <mikemacharia39@gmail.com>
2022-07-03 21:54:53 +0300

    implement order model

--- src/main/java/com/mikehenry/interceptor/model/Order.java
+++ src/main/java/com/mikehenry/interceptor/model/Order.java
@@ -1,5 +1,12 @@
 package com.mikehenry.interceptor.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Order {
     private String itemName;
     private int quantity;
