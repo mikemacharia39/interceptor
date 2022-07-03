a19d516421485a15abe365ae4aa6e168e7ccbac5
mikemacharia39 <mikemacharia39@gmail.com>
2022-07-03 22:18:38 +0300

    update order model

--- src/main/java/com/mikehenry/interceptor/model/Order.java
+++ src/main/java/com/mikehenry/interceptor/model/Order.java
@@ -3,10 +3,12 @@ package com.mikehenry.interceptor.model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@ToString
 public class Order {
     private String itemName;
     private int quantity;
