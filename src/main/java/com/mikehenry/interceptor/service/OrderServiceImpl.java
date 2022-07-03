1bcfd6d08313f8d7c5a14f3f9942ca43a143e2e2
mikemacharia39 <mikemacharia39@gmail.com>
2022-07-03 22:18:16 +0300

    implement order service impl

--- src/main/java/com/mikehenry/interceptor/service/OrderServiceImpl.java
+++ src/main/java/com/mikehenry/interceptor/service/OrderServiceImpl.java
@@ -1,2 +1,14 @@
-package com.mikehenry.interceptor.service;public class OrderServiceImpl {
+package com.mikehenry.interceptor.service;
+
+import com.mikehenry.interceptor.model.Order;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class OrderServiceImpl implements OrderService{
+    @Override
+    public void createOrder(Order order) {
+
+    }
 }
