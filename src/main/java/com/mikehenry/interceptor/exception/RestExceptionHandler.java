0e59535e18d63aa54cfd8ffb35f9cf85a0f33b1e
mikemacharia39 <mikemacharia39@gmail.com>
2022-07-05 20:12:28 +0300

    the perfect exception handler

--- src/main/java/com/mikehenry/interceptor/exception/RestExceptionHandler.java
+++ src/main/java/com/mikehenry/interceptor/exception/RestExceptionHandler.java
@@ -1,2 +1,23 @@
-package com.mikehenry.interceptor.exception;public class RestExceptionHandler {
+package com.mikehenry.interceptor.exception;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RequiredArgsConstructor
+@RestControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private final MessageSource messageSource;
+
+    @ExceptionHandler(OrderNotAllowedException.class)
+    public final ResponseEntity<OrderNotAllowedException> handleOrderNotAllowed(OrderNotAllowedException exception, WebRequest webRequest) {
+        return new ResponseEntity<>(exception, HttpStatus.NOT_ACCEPTABLE);
+    }
+
 }
